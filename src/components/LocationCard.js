e724f5c62ef0707e1df2108aa78abb42dc5e86f7
May Ng <mngmay@gmail.com>
2019-07-26 09:15:45 -0700

    Location tab and cards appear. wip - styling

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
 export default function LocationCard({ location }) {
   const { name, type, dimension, residents } = location;
   console.log(location);
 
-  // image={image}
-  return <span>todo: location</span>;
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Description>
+          {type} - {dimension}
+        </Card.Description>
+        <Card.Meta>Residents: {residents.length}</Card.Meta>
+      </Card.Content>
+    </Card>
+  );
 }
