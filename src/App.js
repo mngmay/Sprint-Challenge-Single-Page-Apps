3dae714e905b0d4a1502d061db38f53a058664ff
May Ng <mngmay@gmail.com>
2019-07-26 11:24:27 -0700

    Created useLocalStorage hook and onSearch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import AppRouter from "./components/AppRouter.js";
 import SearchForm from "./components/SearchForm";
 
 export default function App() {
+  const [name, setName] = useState(null);
+
+  const onSearch = (event, query) => {
+    event.preventDefault();
+    setName(query.name);
+  };
+
   return (
     <main>
       <Header />
+      <SearchForm onSearch={onSearch} />
       <TabNav />
       <AppRouter />
-      <SearchForm />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
+import { setNormalizedScrollLeft } from "normalize-scroll-left";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
