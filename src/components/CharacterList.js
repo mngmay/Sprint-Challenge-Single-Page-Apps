7429e230593da323c7178ecacb9d1e2c662a5fbc
May Ng <mngmay@gmail.com>
2019-07-26 08:42:56 -0700

    Added axios request to CharacterList and mapped to create cards. Passed props to CharacterCard.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then(response => {
+        // console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }, []);
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return (
+    <section className="character-list grid-view">
+      {characters.map(char => (
+        <CharacterCard characater={char} />
+      ))}
     </section>
-
+  );
 }
